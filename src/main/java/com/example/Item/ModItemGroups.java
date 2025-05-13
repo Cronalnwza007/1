3702f41dbdf38d4a27f9fe380a05e311dfddd4cf
gameo <gameover1029ran@gmail.com>
2025-05-13 15:05:06 +0700

    asdfdsaf

--- src/main/java/com/example/Item/ModItemGroups.java
+++ src/main/java/com/example/Item/ModItemGroups.java
@@ -2,20 +2,23 @@ package com.example.Item;
 
 import com.example.TemplateMod;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import static com.example.Item.ModItems.PINK_GARNET;
+
 public class ModItemGroups {
-    public static final ItemGroups PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+    public static final ItemGroup PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(TemplateMod.MOD_ID, "pink_garnet_items"),
-            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_GARNET))
-                    .displayName(Text.translatable("itemgroup.templatemod.pink_garnet_items"))
-                    .entries(((displayContext, entries) -> {
-                        entries.add(ModItems.PINK_GARNET);
+            FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(PINK_GARNET))
+                    .displayName(Text.translatable("itemgroup.template-mod.pink_garnet_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(PINK_GARNET);
                     }).build());
 
     public static void registerItemGroups() {
